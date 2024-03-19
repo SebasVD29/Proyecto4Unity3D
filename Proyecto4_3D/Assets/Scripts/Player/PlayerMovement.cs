95129d701039fd3d4eacd4314543f643fc0d82aa
SebasVD29 <100324444+SebasVD29@users.noreply.github.com>
2024-03-19 14:16:44 -0600

    StartPlayerJump

--- Proyecto4_3D/Assets/Scripts/Player/PlayerMovement.cs
+++ Proyecto4_3D/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,12 +8,23 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
+    [Header("MovementJump")]
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
 
     [Header("Componentes")]
     public Transform orientacion;
     Rigidbody playerRB;
     Vector2 inputMove;
     Vector3 moveDirection;
+
+    [Header("GroundChecks")]
+    public float playerHeigth;
+    public float groundDrag;
+    public LayerMask groundLayer;
+    bool grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -24,23 +35,55 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        HandleGrad();
+        SpeedControl();
     }
     private void FixedUpdate()
     {
         MovePlayer();
     }
-    public void Move(InputAction.CallbackContext context)
-    {
-        inputMove = context.ReadValue<Vector2>();
-    }
 
+    private bool isGrounded()
+    {
+       // grounded = Physics.Raycast(transform.position, Vector3.down, playerHeigth * 0.5f + 0.2f, groundLayer);
+        return Physics.Raycast(transform.position, Vector3.down, playerHeigth * 0.5f + 0.2f, groundLayer);
+    }
     void MovePlayer()
     {
         moveDirection = orientacion.forward * inputMove.y + orientacion.right * inputMove.x;
         playerRB.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
 
     } 
+
+    void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(playerRB.velocity.x, 0f, playerRB.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            playerRB.velocity = new Vector3(limitedVel.x, playerRB.velocity.y, limitedVel.z);
+        }
+    }
+
+
+    void HandleGrad()
+    {
+        if (isGrounded())
+        {
+            playerRB.drag = groundDrag;
+        }
+        else
+        {
+            playerRB.drag = 0;
+        }
+    }
+
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        inputMove = context.ReadValue<Vector2>();
+    }
 
 
 }
