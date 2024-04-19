47e56bc9e28e75985f647d2652a73033075ee9d3
isabellaaaaa03 <100324470+isabellaaaaa03@users.noreply.github.com>
2024-04-18 18:56:40 -0600

    Cambios Lanzar Objetos

--- Proyecto4_3D/Assets/Scripts/Otros/Throwing.cs
+++ Proyecto4_3D/Assets/Scripts/Otros/Throwing.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 
 public class Throwing : MonoBehaviour
 {
-    [Header("Reference")]
-
+    [Header("References")]
     public Transform cam;
     public Transform attackPoint;
     public GameObject objectToThrow;
 
+
     [Header("Settings")]
     public int totalThrows;
-    public float throwCoolDown;
+    public float throwCooldown;
+
 
     [Header("Throwing")]
     public KeyCode throwKey = KeyCode.Mouse0;
@@ -34,41 +35,28 @@ public class Throwing : MonoBehaviour
             Throw();
         }
     }
+
     private void Throw()
     {
         readyToThrow = false;
 
-        //Instanciar el objeto a tirar
         GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
 
-        //Obtener el rigidbody del componente
         Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
 
+        Vector3 forceToAdd = cam.transform.forward * throwForce + transform.up * throwUpwardForce;
 
-        //Calcular la direccion 
-        Vector3 forceDirection = cam.transform.forward;
-
-        RaycastHit hit;
-        
-
-        if(Physics.Raycast(cam.position, cam.forward, out hit, 500f))
-        {
-            forceDirection = (hit.point - attackPoint.position).normalized;
-        }
-
-        //A;adir fuerza
-        Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
-
-        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse );
 
         totalThrows--;
 
-        //Implementar funcion ThrowCoolDown
-        Invoke(nameof(ResetThrow), throwCoolDown);
+        Invoke(nameof(ResetThrow), throwCooldown);
+
     }
 
     private void ResetThrow()
     {
-        readyToThrow = true; 
+        readyToThrow = true;
     }
+         
 }
---
+++ Proyecto4_3D/Assets/Scripts/Player/Sliding.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sliding : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform playerObj;
+    private Rigidbody rb;
+      
+
+
+}
