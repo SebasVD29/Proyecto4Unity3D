f559aa848b77c9dfbbefb901226430ec7785d253
isabellaaaaa03 <100324470+isabellaaaaa03@users.noreply.github.com>
2024-04-18 19:16:03 -0600

    Slide

--- Proyecto4_3D/Assets/Scripts/Player/Sliding.cs
+++ Proyecto4_3D/Assets/Scripts/Player/Sliding.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Sliding : MonoBehaviour
 {
@@ -8,7 +10,58 @@ public class Sliding : MonoBehaviour
     public Transform orientation;
     public Transform playerObj;
     private Rigidbody rb;
-      
+    //private PlayerMovementAdvanced pm;
 
+    [Header("Sliding")]
+    public float maxSlideTime;
+    public float slideForce;
+    public float slideTimer;
+
+    public float slideYScale;
+    private float startYScale;
+
+    [Header("Input")]
+    public KeyCode slideKey = KeyCode.LeftControl;
+    private float horizontalInput;
+    private float verticalInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //pm = GetComponent<PlayerMovementAdvanced>();
+
+        startYScale = playerObj.localScale.y;
+
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(slideKey) && (horizontalInput != 0 || verticalInput != 0))
+        {
+            StartSlide();
+        }
+
+        /*if (Input.GetKeyUp(slideKey) && sliding)
+            StopSlide();*/
+    }
+
+
+    private void StartSlide()
+    {
+        
+    }
+
+    private void SlidingMovement()
+    {
+
+    }
+
+    private void StopSlide()
+    {
+
+    }
 
 }
